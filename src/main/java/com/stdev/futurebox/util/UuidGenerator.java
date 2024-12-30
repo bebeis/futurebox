6502ba430b3f9a9c149078c2bc59a6f6e2b14794
DawnGlow <ferrater1013@gmail.com>
2024-12-30 11:15:19 +0900

    fix(-): 커밋 오류

--- src/main/java/com/stdev/futurebox/util/UuidGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.stdev.futurebox.util;
-
-import java.util.UUID;
-
-public class UuidGenerator {
-    public static UUID generateUuid() {
-        return java.util.UUID.randomUUID();
-    }
-}
