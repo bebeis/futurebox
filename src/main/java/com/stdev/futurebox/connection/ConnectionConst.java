e765e424912278eb74068c6510cdec40c545aad2
DawnGlow <ferrater1013@gmail.com>
2024-12-30 11:11:27 +0900

    fix(-): gitignore

--- src/main/java/com/stdev/futurebox/connection/ConnectionConst.java
+++
@@ -1,7 +0,0 @@
-package com.stdev.futurebox.connection;
-
-public class ConnectionConst {
-    public static final String URL = "jdbc:postgresql://ep-proud-haze-a1h6t5b9.ap-southeast-1.aws.neon.tech/FutureBox?sslmode=require";
-    public static final String USERNAME = "FutureBox_owner";
-    public static final String PASSWORD = "2b5QPBhmkMRx";
-}
