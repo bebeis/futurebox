403a22ce77ea94591078fbd4cb6fbb51269be6c2
DawnGlow <ferrater1013@gmail.com>
2024-12-30 18:36:42 +0900

    fix(DBConnection): 환경변수 분리

--- src/main/java/com/stdev/futurebox/connection/ConnectionConst.java
+++ src/main/java/com/stdev/futurebox/connection/ConnectionConst.java
@@ -1,7 +1,7 @@
 package com.stdev.futurebox.connection;
 
 public class ConnectionConst {
-    public static final String URL = "jdbc:postgresql://ep-proud-haze-a1h6t5b9.ap-southeast-1.aws.neon.tech/FutureBox?sslmode=require";
-    public static final String USERNAME = "FutureBox_owner";
-    public static final String PASSWORD = "2b5QPBhmkMRx";
+    public static final String URL = System.getenv("DB_URL");
+    public static final String USERNAME = System.getenv("DB_USERNAME");
+    public static final String PASSWORD = System.getenv("DB_PASSWORD");
 }
